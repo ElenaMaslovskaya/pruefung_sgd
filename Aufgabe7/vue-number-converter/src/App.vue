eba4bd9447f99be26e44fccc4a0363e9ef7ac08a
Elena Maslovskaya <elena.maslovski@yandex.ru>
2023-09-12 11:03:21 +0200

    feat: finish project

--- Aufgabe7/vue-number-converter/src/App.vue
+++ Aufgabe7/vue-number-converter/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NumberConverter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberConverter from './components/NumberConverter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NumberConverter,
   }
 }
 </script>
@@ -19,8 +20,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #121212;
+  color: #ffffff;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
 }
 </style>
---
+++ Aufgabe7/vue-number-converter/src/components/NumberConverter.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Dezimal zu Binär- und Hexadezimalumrechner</h1>
+        <label for="decimalInput">Geben Sie eine Dezimalzahl ein:</label>
+        <input id="decimalInput" v-model="decimalValue" @input="convert" @keypress="validateInput"/>
+        <p>Binär: {{ binaryValue }}</p>
+        <p>Hexadezimal: {{ hexValue }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        decimalValue: "",
+        binaryValue: "",
+        hexValue: "",
+        };
+    },
+    methods: {
+        convert() {
+            const decimal = parseInt(this.decimalValue, 10);
+            if (!isNaN(decimal)) {
+                this.binaryValue = decimal.toString(2);
+                this.hexValue = decimal.toString(16);
+            } else {
+                this.binaryValue = "";
+                this.hexValue = "";
+            }
+        },
+        validateInput(event) {
+            const allowedChars = "0123456789";
+            if (event.key && allowedChars.indexOf(event.key) === -1) {
+            event.preventDefault();
+            }
+        },
+    },
+};
+</script>
+
+<style>
+    h1 {
+        color:lightblue;
+    }
+
+    input {
+        margin-left: 10px;
+    }
+</style>
